6747672d2634b9996b43a9cf82011dcab957bc8e
Marcos <markitosanches@gmail.com>
2023-10-31 16:42:23 -0400

    Seance 16

---
+++ seance16/view/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>{{ title }}</title>
+                <link rel="stylesheet" href="{{path}}css/style.css">
+            </head>
+            <nav>
+            <ul>
+                <li><a href="{{path}}">Home</a></li>
+                <li><a href="{{path}}client">Client</a></li>
+                <li><a href="{{path}}client/create">Client Create</a></li>
+
+            </ul>
+        </nav>
